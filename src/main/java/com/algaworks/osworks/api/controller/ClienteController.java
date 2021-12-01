d4f2296040e5d930d471a2b3c1411111297b4215
Wagner Marçal <wagnermarcal.2008@gmail.com>
2021-12-01 16:47:44 -0300

    Spring Rest - CRUD

--- src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
@@ -1,27 +1,63 @@
 package com.algaworks.osworks.api.controller;
 
 import com.algaworks.osworks.domain.model.Cliente;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.algaworks.osworks.domain.repository.ClienteRepository;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
+@RequestMapping("/clientes")
 public class ClienteController {
-    @GetMapping("/clientes")
-    public List<Cliente> listar() {
-        Cliente cliente1 = new Cliente();
-        cliente1.setId(1L);
-        cliente1.setNome("Wagner");
-        cliente1.setEmail("wagnermarcal@hotmail.com");
-        cliente1.setTelefone("41 - 9 9948-6471");
-        Cliente cliente2 = new Cliente();
-        cliente2.setId(2L);
-        cliente2.setNome("Luis");
-        cliente2.setEmail("luismarcal_2009@hotmail.com");
-        cliente2.setTelefone("41 - 9 9620-1995");
 
-        return Arrays.asList(cliente1, cliente2);
+    private final ClienteRepository clienteRepository;
+
+    public ClienteController(ClienteRepository clienteRepository) {
+        this.clienteRepository = clienteRepository;
     }
+
+    @GetMapping
+    public List<Cliente> listar() {
+        return clienteRepository.findAll();
+    }
+
+    @GetMapping("/{clienteId}")
+    public ResponseEntity<Cliente> buscar(@PathVariable Long clienteId) {
+        Optional<Cliente> cliente = clienteRepository.findById(clienteId);
+        if (cliente.isPresent()) {
+            return ResponseEntity.ok(cliente.get());
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cliente adicionar(@RequestBody Cliente cliente) {
+        return clienteRepository.save(cliente);
+    }
+
+    @PutMapping("/{clienteId}")
+    public ResponseEntity<Cliente> atualizar(@PathVariable Long clienteId,
+                                             @RequestBody Cliente cliente) {
+        if (!clienteRepository.existsById(clienteId)) {
+            return ResponseEntity.notFound().build();
+        }
+        cliente.setId(clienteId);
+        clienteRepository.save(cliente);
+        return ResponseEntity.ok(cliente);
+    }
+
+    @DeleteMapping("/{clienteId}")
+    public ResponseEntity<Void> remover(@PathVariable Long clienteId) {
+        if (!clienteRepository.existsById(clienteId)) {
+            return ResponseEntity.notFound().build();
+        }
+        clienteRepository.deleteById(clienteId);
+        return ResponseEntity.noContent().build();
+    }
+
 }
--- src/main/java/com/algaworks/osworks/domain/model/Cliente.java
+++ src/main/java/com/algaworks/osworks/domain/model/Cliente.java
@@ -1,12 +1,19 @@
 package com.algaworks.osworks.domain.model;
 
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
 public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String nome;
     private String email;
+
+    @Column(name = "fone")
     private String telefone;
-
-
 
     public Long getId() {
         return id;
@@ -38,5 +45,18 @@ public class Cliente {
 
     public void setTelefone(String telefone) {
         this.telefone = telefone;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cliente cliente = (Cliente) o;
+        return Objects.equals(id, cliente.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
